856a4603429a6a8a01e5872bb921ec7ed37cf4cc
Izabella Hermano <izabellahermano1@gmail.com>
2024-10-08 09:26:41 -0300

    String

---
+++ src/aula02/Exemplos/Exemplo06.java
@@ -0,0 +1,10 @@
+package aula02.Exemplos;
+
+public class Exemplo06 {
+    public static void main(String[] args) {
+        String varString = "Esse é o texto na String \nEsse texto está na outra linha"+
+                                "\n\teste texto tem tabulação";
+
+        System.out.println(varString);
+    }
+}
