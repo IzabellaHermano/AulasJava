50aeb5ec1bc7df87ac77b3d16718f3fd35f968b0
Aluno <pedrosantos471572@gmail.com>
2024-10-08 09:07:13 -0300

    Casting

---
+++ src/aula02/Exemplos/Exemplo05.java
@@ -0,0 +1,13 @@
+package aula02.Exemplos;
+
+public class Exemplo05 {
+    public static void main(String[] args) {
+        byte varByte = 127;
+        short varShort = varByte;
+        int varInt = varShort;
+        long varLong = varInt;
+
+
+        System.out.println(varByte);
+    }
+}
