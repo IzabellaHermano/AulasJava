dc0bdbd8dafe23d403b516f1400358dc148d5ccd
Izabella Hermano <izabellahermano1@gmail.com>
2024-10-11 16:55:13 -0300

    Continuação dos exercícios

---
+++ src/aula02/Exercicios/Exercicio08.java
@@ -0,0 +1,14 @@
+package aula02.Exercicios;
+
+public class Exercicio08 {
+    public static void main(String[] args) {
+        float total, altura = 6.5f, largura = 12.5f;
+
+        total= altura * largura;
+
+        System.out.println(" A área é " +total);
+
+
+
+    }
+}
