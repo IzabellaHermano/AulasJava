59165eba91cbdfbd591efac1c17fa3ae82f49c4e
Izabella Hermano <izabellahermano1@gmail.com>
2024-10-16 08:40:53 -0300

    Estruturas de Decisão

---
+++ src/aula04/Exemplos/Exemplo01.java
@@ -0,0 +1,17 @@
+package aula04.Exemplos;
+
+import java.util.Scanner;
+
+public class Exemplo01 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int x;
+        System.out.println("Digite um numero");
+        x=scanner.nextInt();
+        if (x >= 30)
+        System.out.println("numero maior  ou igual a 30");
+         else {
+        System.out.println("numero menor que 30");
+        }
+    }
+}
---
+++ src/aula04/Exercicios/Exercicio01.java
@@ -0,0 +1,7 @@
+package aula04.Exercicios;
+
+public class Exercicio01 {
+    public static void main(String[] args) {
+
+    }
+}
