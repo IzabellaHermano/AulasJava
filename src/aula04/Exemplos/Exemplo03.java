57ad40d8399518de1d53ba21a969e058730418be
Izabella Hermano <izabellahermano1@gmail.com>
2024-10-18 10:12:19 -0300

    Estruturas de Decisão

--- src/aula04/Exemplos/Exemplo03.java
+++ src/aula04/Exemplos/Exemplo03.java
@@ -4,6 +4,26 @@ import java.util.Scanner;
 
 public class Exemplo03 {
     public static void main(String[] args) {
+        int idade;
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Digite a sua idade");
+        idade = scanner.nextInt();
+
+        if (idade > 0 && idade <= 12) {
+            System.out.println("Você é criança");
+
+        } else if (idade > 12 && idade <= 17) {
+            System.out.println("Você é adolescente");
+
+        } else if (idade > 17 && idade < 65) {
+            System.out.println("Você é adulto");
+
+        }else if (idade >= 65 ) {
+            System.out.println("Você é idoso");
+        }else {
+            System.out.println("Idade inválida");
+        }
+        scanner.close();
     }
 }
---
+++ src/aula04/Exemplos/Exemplo04.java
@@ -0,0 +1,7 @@
+package aula04.Exemplos;
+
+public class Exemplo04 {
+    public static void main(String[] args) {
+
+    }
+}
