845e4e6bde87e719e5dbc89250a8eacd7519ebdf
Izabella Hermano <izabellahermano1@gmail.com>
2024-10-18 11:16:08 -0300

    Estruturas de Decisão

--- src/aula04/exercicios/Exercicio01.java
+++ src/aula04/exercicios/Exercicio01.java
@@ -1,7 +1,20 @@
 package aula04.exercicios;
 
+import java.util.Scanner;
 public class Exercicio01 {
     public static void main(String[] args) {
+        int idade;
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Informe sua idade para ter acesso ao sistema");
+        idade= scanner.nextInt();
+
+        if (idade >= 18){
+            System.out.println("Acesso permitido");
+        }else{
+            System.out.println("Acesso negado");
+        }
+
+    scanner.close();
     }
 }
---
+++ src/aula04/exercicios/Exercicio02.java
@@ -0,0 +1,20 @@
+package aula04.exercicios;
+
+import java.util.Scanner;
+public class Exercicio02 {
+    public static void main(String[] args) {
+        int valor;
+        double desconto = 0.1;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Informe o valor da seu a compra");
+        valor = scanner.nextInt();
+
+        if (valor >= 100){
+
+
+        }
+
+    }
+}
