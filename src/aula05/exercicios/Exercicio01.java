0e33091562e024fee20f14a69a0a01913aafe507
Izabella Hermano <izabellahermano1@gmail.com>
2024-11-01 11:14:53 -0300

    Exercícios

--- src/aula05/exercicios/Exercicio01.java
+++ src/aula05/exercicios/Exercicio01.java
@@ -1,7 +1,13 @@
 package aula05.exercicios;
 
-public class Exercicio01 {
-    public static void main(String[] args) {
+import java.util.Timer;
 
+public class Exercicio01 {
+    public static void main(String[] args) throws InterruptedException {
+        for ( int x = 10; x >= 1; x--){
+            System.out.println(x);
+            Thread.sleep(2000);
+        }
+        System.out.println("FIM!!");
     }
 }
