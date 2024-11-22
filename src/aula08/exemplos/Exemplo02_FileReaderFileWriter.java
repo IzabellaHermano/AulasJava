b4272ded380d0aaaec41c9b5ff23ff80457af401
Izabella Hermano <izabellahermano1@gmail.com>
2024-11-22 10:47:46 -0300

    Manipulação de Arquivos

---
+++ src/aula08/exemplos/Exemplo02_FileReaderFileWriter.java
@@ -0,0 +1,29 @@
+package aula08.exemplos;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Exemplo02_FileReaderFileWriter {
+    public static void main(String[] args) {
+        File arquivo = new File("src\\aula08\\exemplos\\arquivo.txt");
+
+        try {
+
+            FileWriter fileWriter = new FileWriter(arquivo);
+            fileWriter.write("Texto gravado no arquivo");
+            fileWriter.close();
+
+            FileReader fileReader = new FileReader(arquivo);
+            int caracter;
+            while ((caracter = fileReader.read()) != -1){
+                System.out.print((char)caracter);
+            }
+            fileReader.close();
+
+        } catch (IOException e) {;
+            throw new RuntimeException(e);
+        }
+    }
+}
