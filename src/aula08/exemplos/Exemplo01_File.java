64299596c1220c80aac623247a0b6f7debb05a36
Izabella Hermano <izabellahermano1@gmail.com>
2024-11-22 10:11:39 -0300

    Manipulação de Arquivos

---
+++ src/aula08/exemplos/Exemplo01_File.java
@@ -0,0 +1,9 @@
+package aula08.exemplos;
+
+import java.io.File;
+
+public class Exemplo01_File {
+    public static void main(String[] args) {
+        File arquivo = new File();
+    }
+}
---
+++ src/aula08/exercicios/Exercicio01.java
@@ -0,0 +1,7 @@
+package aula08.exercicios;
+
+public class Exercicio01 {
+    public static void main(String[] args) {
+
+    }
+}
