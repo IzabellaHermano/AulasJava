3aa0207950cc70e22ded3e69445dc701aee7d6ea
Izabella Hermano <izabellahermano1@gmail.com>
2024-11-29 11:03:25 -0300

    Resolução de Desafios

--- src/desafios/Desafio02_Aula07.java
+++ src/desafios/Desafio02_Aula07.java
@@ -1,7 +1,46 @@
 package desafios;
 
+import java.util.Scanner;
+
 public class Desafio02_Aula07 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String menu= """
+                ________________________________________________________________
+                |   Escolha uma Opição:                                         |
+                |       1- Exibir Cadastro Completo                             |
+                |       2- Cadastrar Usuário                                    |
+                |       3- Atualizar Usuário                                    |
+                |       4- Deletar Usuário                                      |
+                |       5- Sair                                                 |
+                |_______________________________________________________________|
+                """;
+        System.out.println(menu);
+        int opcao = scanner.nextInt();;
+        scanner.nextLine();
 
+        switch (opcao){
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+            default:
+                System.out.println("Opção inválida.");
+        }
+
+    }
+    public static void exibirUsuarios(){
+    }
+    public static void cadastrarUsuarios(){
+    }
+    public static void atualizarUsuarios(){
+    }
+    public static void deletarUsuarios(){
     }
 }
