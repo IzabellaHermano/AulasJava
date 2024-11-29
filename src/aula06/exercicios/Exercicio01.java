bfae2f9c0a31fba8301576ab408aba31fd4b7613
Izabella Hermano <izabellahermano1@gmail.com>
2024-11-29 09:26:44 -0300

    Matriz

--- src/aula06/exercicios/Exercicio01.java
+++ src/aula06/exercicios/Exercicio01.java
@@ -1,7 +1,24 @@
 package aula06.exercicios;
 
+import java.util.Scanner;
+
 public class Exercicio01 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int [] numeros = new int[5];
+        int soma = 0;
+
+        System.out.print("Digite 5 números inteiros:");
+        
+        for(int i=0; i < numeros.length; i++){
+            System.out.println("Numero " + (i +1)+ ":");
+            numeros[i]= scanner.nextInt();
+            soma += numeros[i];
+        }
+        System.out.println("A soma dos numeros é "+ soma);
+        
+        scanner.close();
+        
 
     }
 }
