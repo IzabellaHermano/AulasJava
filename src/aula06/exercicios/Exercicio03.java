2d9e21f2e11efbad99510ec0b942e81a4ca7bb35
Izabella Hermano <izabellahermano1@gmail.com>
2024-12-03 20:17:14 -0300

    Vetores e Matrizes

--- src/aula06/exercicios/Exercicio03.java
+++ src/aula06/exercicios/Exercicio03.java
@@ -1,7 +1,9 @@
 package aula06.exercicios;
 
+import java.util.Scanner;
+
 public class Exercicio03 {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
     }
 }
