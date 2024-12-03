79c6bd0b636031b8c37989009b597b9c33380748
Izabella Hermano <izabellahermano1@gmail.com>
2024-12-03 20:13:35 -0300

    Vetores e Matrizes

--- src/aula06/exercicios/Exercicio02.java
+++ src/aula06/exercicios/Exercicio02.java
@@ -1,8 +1,27 @@
 package aula06.exercicios;
 
+import java.util.Scanner;
+
 public class Exercicio02 {
     public static void main(String[] args) {
-        String [] letras = new String[6];
+        Scanner scanner = new Scanner(System.in);
+        String[] letras = {"c", "o", "e", "l", "h", "o"};
 
+        System.out.print("Digite uma letra:");
+        String resposta = scanner.next();
+        boolean presente = true;
+
+        for (int i = 0; i < letras.length; i++) {
+            if (letras[i].equals(resposta))
+            {
+                System.out.println("A letra " + resposta + "está presente no vetor e se encontra na posição " + i + " do vetor");
+            }
+            presente = false;
+
+        }
+        if (!presente) {
+            System.out.println("Letra não encontrada no vetor");
+        }
+        scanner.close();
     }
 }
