1c7367661804219ceab22529c4956aa709cb06cd
Izabella Hermano <izabellahermano1@gmail.com>
2024-12-04 08:51:47 -0300

    Desafio

--- src/desafios/Desafio02_Aula07.java
+++ src/desafios/Desafio02_Aula07.java
@@ -3,8 +3,15 @@ package desafios;
 import java.util.Scanner;
 
 public class Desafio02_Aula07 {
+
+//Variável global
+
+    static String [] cabecalho = {"ID","NOME","TELEFONE","EMAIL"};
+    static String [][] matrizCadastro;
+    static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+
         String menu= """
                 _________________________________________________________________
                 |   Escolha uma Opição:                                         |
@@ -45,10 +52,38 @@ public class Desafio02_Aula07 {
 
     }
     public static void exibirUsuarios(){
-        System.out.println("exibirUsuarios");
+
     }
     public static void cadastrarUsuarios(){
-        System.out.println("cadastrarUsuarios");
+
+        System.out.print("Informe a quantidade de usuários a serem cadastrados:");
+        int qntUsuario = scanner.nextInt();
+        scanner.nextLine();
+
+        matrizCadastro = new String[qntUsuario+1][cabecalho.length];
+        matrizCadastro [0] = cabecalho;
+
+        for (int linha = 1; linha <=qntUsuario ;linha++){
+            System.out.println("-----------------------------------------------------");
+            System.out.println("Preencha os dados a seguir:");
+            System.out.println("Cadastro do usuário "+linha);
+
+            System.out.print("ID:"+linha);
+            matrizCadastro[linha][0] =String.valueOf(linha);
+            scanner.nextLine();
+
+            System.out.print("NOME:");
+            matrizCadastro [linha][1] = scanner.nextLine();
+
+
+            System.out.print("TELEFONE:");
+            matrizCadastro [linha][2]= scanner.nextLine();
+
+
+            System.out.print("EMAIL:");
+            matrizCadastro [linha][3]= scanner.nextLine();
+
+        }
     }
     public static void atualizarUsuarios(){
         System.out.println("atualizarUsuarios");
