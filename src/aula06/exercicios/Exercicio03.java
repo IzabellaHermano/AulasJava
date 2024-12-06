3e774f67cd47993d904f80744b3590f2314f0292
Izabella Hermano <izabellahermano1@gmail.com>
2024-12-06 19:45:01 -0300

    Vetores

--- src/aula06/exercicios/Exercicio03.java
+++ src/aula06/exercicios/Exercicio03.java
@@ -5,5 +5,24 @@ import java.util.Scanner;
 public class Exercicio03 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        boolean[] posicao = new boolean[8];
+        int total = 0;
+
+        System.out.println("Digite 8 variações de TRUE or FALSE: ");
+
+        for (int i = 0; i < posicao.length; i++) {
+            System.out.println("TRUE OR FALSE: ");
+            posicao[i] = scanner.nextBoolean();
+
+            if (posicao[i]) {
+                total++;
+            }
+
+        }
+        System.out.println(" A quantidade de TRUE que foram digitados foram "+ total);
+
+        scanner.close();
+
     }
 }
---
+++ src/aula06/exercicios/Exercicio04.java
@@ -0,0 +1,24 @@
+package aula06.exercicios;
+
+import java.util.Scanner;
+
+public class Exercicio04 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double [] notas =new double[4];
+        double soma= 0;
+
+        System.out.println("Digite as notas: ");
+        for (int i = 0; i < notas.length ; i++) {
+            System.out.print("NOTA: ");
+            notas[i] = scanner.nextDouble();
+            soma += notas[i];
+
+        }
+        double media = soma / notas.length;
+
+        System.out.println(" A média das notas são: "+media);
+
+    }
+}
