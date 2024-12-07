5bae58dded1bf53a1465cd168aa3b40907561ae5
Izabella Hermano <izabellahermano1@gmail.com>
2024-12-06 21:25:11 -0300

    Invocando Funções

--- src/aula07/exercicios/Exercicio01.java
+++ src/aula07/exercicios/Exercicio01.java
@@ -3,5 +3,12 @@ package aula07.exercicios;
 public class Exercicio01 {
     public static void main(String[] args) {
 
+        imprimirSaudacao();
+
+    }
+
+    private static void imprimirSaudacao() {
+        System.out.println( "BEM-VINDO AO SISTEMA!");
+
     }
 }
