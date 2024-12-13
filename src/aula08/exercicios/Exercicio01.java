b0d42224d0f403decb172a48a023bf4419ec3a0a
Izabella Hermano <izabellahermano1@gmail.com>
2024-12-13 18:45:09 -0300

    Manipulação de Arquivos

--- src/aula08/exercicios/Exercicio01.java
+++ src/aula08/exercicios/Exercicio01.java
@@ -1,7 +1,34 @@
 package aula08.exercicios;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Exercicio01 {
     public static void main(String[] args) {
+        Scanner scanner =new Scanner(System.in);
+        File arquivo = new File("src\\aula08\\exercicios\\alunos.txt");
+
+        try {
+            FileWriter escrever = new FileWriter(arquivo);
+
+            System.out.println("Insira o Nome dos Alunos:");
+            for (int i = 1; i <=5 ; i++) {
+
+                System.out.print("Aluno "+i+": ");
+                String nomeAlunos = scanner.nextLine();
+                escrever.write(nomeAlunos+"\n");
+
+            }
+            escrever.close();
+            System.out.println("Nomes Salvos com Sucesso");
+
+
+        } catch (IOException e) {
+            System.out.println("Error em Salvar Arquivo");
+            e.printStackTrace();
+        }
 
     }
 }
