13ce09d008488d9849e0085a5ff5efb4bc80817b
Izabella Hermano <izabellahermano1@gmail.com>
2024-12-13 19:03:31 -0300

    Manipulação de Arquivos

---
+++ src/aula08/exercicios/Exercicio02.java
@@ -0,0 +1,24 @@
+package aula08.exercicios;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Exercicio02 {
+    public static void main(String[] args) {
+        String arquivo ="src\\aula08\\exercicios\\alunos.txt";
+
+        try(BufferedReader leitura = new BufferedReader(new FileReader (arquivo))) {
+            String linha;
+            System.out.println("Conteúdo do Arquivo:");
+            while ((linha = leitura.readLine())!= null){
+                System.out.println(linha);
+            }
+
+        } catch (IOException e){
+            System.out.println("Erro em Realizar Leitura");
+
+        }
+    }
+}
